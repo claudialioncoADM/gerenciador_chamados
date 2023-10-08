4b2d2ef9362224805f01b6467f763b090158d095
claudialioncoADM <claudialionco@gmail.com>
2023-10-07 23:44:00 -0300

    Configuração de email e tela inicial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,16 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div >
+        <x-welcome />
+
+    </div>
+
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
             </div>
         </div>
-    </div>
+    </div> --}}
 </x-app-layout>
